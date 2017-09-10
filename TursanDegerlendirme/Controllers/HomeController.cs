c6b1ac9d2e627467cb1368c1d4872305182d993f
mehmet oguz dogan <th3__rocker@hotmail.com>
2017-09-10 19:29:12 +0300

    CPU, RAM ve İnternet işlemleri hesaplandı

--- TursanDegerlendirme/Controllers/HomeController.cs
+++ TursanDegerlendirme/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
+using System.Management.Instrumentation;
+using System.Management;
 
 namespace TursanDegerlendirme.Controllers
 {
@@ -10,6 +15,31 @@ namespace TursanDegerlendirme.Controllers
     {
         public ActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string Name = "https://www.google.com.tr")
+        {
+            Uri URL = new Uri(Name);
+            WebClient wc = new WebClient();
+            double starttime = Environment.TickCount;
+            wc.DownloadFile(URL, Server.MapPath("file/speedtest.txt"));
+            double endtime = Environment.TickCount;
+            double secs = Math.Floor(endtime - starttime) / 1000;
+            double secs2 = Math.Round(secs, 0);
+            double kbsec = Math.Round(1024 / secs);
+            TempData["rate"] = kbsec ;
+            PerformanceCounter availableBytes = new PerformanceCounter("Memory", "Available MBytes", true);
+            float numBytes = availableBytes.NextValue();
+            PerformanceCounter cpuCounter = new PerformanceCounter();
+            cpuCounter.CategoryName = "Processor";
+            cpuCounter.CounterName = "% Processor Time";
+            cpuCounter.InstanceName = "_Total";
+            var unused = cpuCounter.NextValue();
+            System.Threading.Thread.Sleep(1000);
+            TempData["cpu"] = (cpuCounter.NextValue());
+            TempData["ram"] = numBytes.ToString();
+            availableBytes.Close();
             return View();
         }
 
